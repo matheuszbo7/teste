a8d7995c6bcff5cae34269787c6ccb5944f9a5eb
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2024-03-07 10:15:37 -0300

    dse

---
+++ js.js
@@ -0,0 +1,20 @@
+let titulo = document.getElementById("titulopagina");
+console.log(titulo);
+
+document.getElementById("meup").textContent = "novo conteudo de texto";
+
+document.querySelector("img").setAttribute("src", "imagem_nova.jpg");
+
+document.getElementById("meue").style.backgroundColor = "green";
+
+document.getElementById("meub").addEventListener("click", function(){
+    document.body.style.backgroundColor = "black"
+    alert("voce clicou no botao");
+});
+
+
+
+
+
+    
+ 
